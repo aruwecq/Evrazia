5eb1221eb29825e0380e84d86ec3776c7024a602
Пользователь <ff8416402@gmail.com>
2026-01-29 17:48:46 +0600

    import

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -2,9 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import { Layout } from "../widgets/Layout/Layout";
 import { Router } from "../pages/route";
 import { Home } from "../pages/landing/ui/Home";
-import  NewsPages  from "../pages/landing/pages/news/NewsPages"
 import  Banner  from "../pages/landing/ui/banner/Banner";
-import Cafe from "../pages/landing/pages/cafe/Cafe";
 import Events from "../pages/landing/pages/events/Events";
 export const myRouter = createBrowserRouter([
   {
@@ -18,14 +16,6 @@ export const myRouter = createBrowserRouter([
         {
         path: "/banner",
         element: <Banner/>
-        },
-        {
-          path: "/news",
-          element: <NewsPages/>
-        },
-        {
-          path: "/cafe",
-          element: <Cafe/>
         },
         {
           path: "/events",
--- src/pages/route.tsx
+++ src/pages/route.tsx
@@ -1,9 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Layout } from "../widgets/Layout/Layout";
+import Cafe from "./landing/pages/cafe/Cafe";
+import NewsPages from "./landing/pages/news/NewsPages";
 export const Router = {    
         path: "",
-        element: <Outlet/>,
-        children:[
-    
+        element: <Layout/>,
+        children:[     
+      {
+          path: "/news",
+          element: <NewsPages/>
+        },
+        {
+          path: "/cafe",
+          element: <Cafe/>
+        }
         ]
 
 
