6a7d2eca9d3a9058b67ff9394ebab9551d1d3499
asanbaevasylbek504-png <asanbaevasylbek504@gmail.com>
2026-02-02 18:27:43 +0600

    kurort1

--- src/pages/route.tsx
+++ src/pages/route.tsx
@@ -1,6 +1,7 @@
 import { Layout } from "../widgets/Layout/Layout";
 import Cafe from "./landing/pages/cafe/Cafe";
 import NewsPages from "./landing/pages/news/NewsPages";
+import { Kurort1 } from "./landing/ui/kurort1/Kurort1";
 export const Router = {    
         path: "",
         element: <Layout/>,
@@ -12,8 +13,10 @@ export const Router = {
         {
           path: "/cafe",
           element: <Cafe/>
+        },
+               {
+          path: "/resorts",
+          element: <Kurort1/>
         }
         ]
-
-
     } 
