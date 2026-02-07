f0579d6a1a49991ce6d4d72f0f6e62aa5ed2ea2d
asanbaevasylbek504-png <asanbaevasylbek504@gmail.com>
2026-02-07 18:35:57 +0600

    slujba

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -9,6 +9,7 @@ import Catalog from "../pages/landing/pages/catalog/Catalog.tsx";
 import Hotel from "../pages/landing/pages/Hotel/Hotel.tsx";
 import Guest from "../pages/landing/pages/Hotel/Guest.tsx";
 import Hotel2 from "../pages/landing/pages/Hotel/Hotel2.tsx";
+import { Slujba } from "../pages/landing/ui/slujba/Slujba.tsx";
 
 const LayoutWithScroll = () => (
   <>
@@ -22,38 +23,43 @@ export const myRouter = createBrowserRouter([
     path: "/",
     element: <LayoutWithScroll />, 
     children: [
-        {
-          path: "/",
-          element: <Home/>,
-        },
-        {
-          path: "/banner",
-          element: <Banner/>
-        },
-        {
-          path: "/events",
-          element: <Events/>
-        },
-        {
-          path: "/ski",
-          element: <Ski/>
-        },
-        {
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+        path: "/banner",
+        element: <Banner/>
+      },
+      {
+        path: "/events",
+        element: <Events/>
+      },
+      {
+        path: "/ski",
+        element: <Ski/>
+      },
+      {
+        path: "/catalog",
+        element: <Catalog/>
+      }, 
+      {
+        path: "/hotel",
+        element: <Hotel/>
+      },
+      {
+        path: "/hotel/rooms",
+        element: <Guest/>
+      },
+      {
+        path: "/hotel/booking",
+        element: <Hotel2/>
+      },
+         {
+        path: "/slu",
+        element: <Slujba/>
+      }
 
-          path: "/catalog",
-          element: <Catalog/>
-        },
-          path: "/hotel",
-          element: <Hotel/>
-        },
-        {
-          path: "/hotel/rooms",
-          element: <Guest/>
-        },
-        {
-          path: "/hotel/booking",
-          element: <Hotel2/>
-        }
     ],
   },
 ]);
